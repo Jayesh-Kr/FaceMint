28e114bb13c93e183146136b59994305e084bc40
Jayesh Krishna <jayeshkr152005@gmail.com>
2025-09-17 00:01:11 +0530

    Added pinata

---
+++ backend/utils/pinata.js
@@ -0,0 +1,20 @@
+import {PinataSDK} from 'pinata';
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: '../.env'
+})
+
+const PINATA_JWT = process.env.PINATA_JWT;
+const pinataGateway = process.env.PINATA_GATEWAY;
+
+if(!PINATA_JWT || !pinataGateway) {
+    console.error("Error from pinata. Missing .env data");
+}
+
+const pinata = new PinataSDK({
+  pinataJwt: PINATA_JWT,
+  pinataGateway: pinataGateway,
+});
+
+export default pinata;
